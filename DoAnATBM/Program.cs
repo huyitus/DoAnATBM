ed2e586e4bd8af4fd1a341dd600260a088ae3c2c
huyitus <huyitus@yahoo.com>
2023-04-05 21:06:00 +0700

    change .net target

--- DoAnATBM/Program.cs
+++ DoAnATBM/Program.cs
@@ -1,17 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
 namespace DoAnATBM
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new FormMain());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FromMain());
         }
     }
 }
