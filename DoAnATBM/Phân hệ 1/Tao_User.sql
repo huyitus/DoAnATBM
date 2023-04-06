fbed659baa120da0cbc85c3313472c8522967042
Nguyễn Thị Hồng Phúc <83939478+nthihongphuc@users.noreply.github.com>
2023-04-06 22:55:04 +0700

    xóa user

--- DoAnATBM/Phân hệ 1/Tao_User.sql
+++ DoAnATBM/Phân hệ 1/Tao_User.sql
@@ -1,5 +1,5 @@
 ----- Tao moi user
-create or replace procedure Grant_NewUser(User_name in varchar2,Pass_Word in nvarchar2)
+create or replace procedure Create_NewUser(User_name in varchar2,Pass_Word in nvarchar2)
 authid current_user 
 as
     Tmp_count int;
@@ -13,9 +13,31 @@ Begin
     END IF;
 End;
 
-//Test
+<<<<<<< HEAD
+----- Xóa user 
+create or replace procedure Drop_User(User_name in varchar2)
+authid current_user
+as
+    Tmp_count int;
+Begin
+    select count(*) into Tmp_count from all_users where username = User_name;
+    if(Tmp_count != 0) then
+        execute immediate('DROP USER'||User_name);
+    else
+    RAISE_APPLICATION_ERROR(-20000,'User khong ton tai');
+    end if;
+End;
+=======
+>>>>>>> 019471cafd1c937f441929df7b7048fdeb1b570c
+--Test
 BEGIN 
-    Grant_NewUser('NV001', 'NV001');
+    Create_NewUser('NV001', 'NV001');
 END;
 
+BEGIN 
+    Drop_User('NV001');
+END;
+exec Drop_User('NV001');
+
+drop user NV001;
 select * from dba_users;
