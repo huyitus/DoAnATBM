1a5b3a851549ecb1e8baa8c4163f3a155f2b9092
nhngoccham <nhnt1811@gmail.com>
2023-04-08 23:56:20 +0700

    fix foreign key

--- DoAnATBM/Phân hệ 1/database.sql
+++ DoAnATBM/Phân hệ 1/database.sql
@@ -41,12 +41,12 @@ create table PHANCONG (
     primary key (MANV, MADA)
 );
 
-alter table NHANVIEN add foreign key (MANQL) references NHANVIEN(MANV);
-alter table NHANVIEN add foreign key (PHG) references PHONGBAN(MAPB);
-alter table PHONGBAN add foreign key (TRPHG) references NHANVIEN(MANV);
-alter table DEAN add foreign key (PHONG) references PHONGBAN(MAPB);
-alter table PHANCONG add foreign key (MADA) references DEAN(MADA);
-alter table PHANCONG add foreign key (MANV) references NHANVIEN(MANV);
+alter table NHANVIEN add constraint FK_NV_QL foreign key (MANQL) references NHANVIEN(MANV);
+alter table NHANVIEN add constraint FK_NV_PHG foreign key (PHG) references PHONGBAN(MAPB);
+alter table PHONGBAN add constraint FK_NV_TRPHG foreign key (TRPHG) references NHANVIEN(MANV);
+alter table DEAN add constraint FK_DA_PHG foreign key (PHONG) references PHONGBAN(MAPB);
+alter table PHANCONG add constraint FK_PC_DA foreign key (MADA) references DEAN(MADA);
+alter table PHANCONG add constraint FK_PC_NV foreign key (MANV) references NHANVIEN(MANV);
 
 insert into PHONGBAN values('PB001', N'Kế hoạch',null);
 insert into PHONGBAN values('PB002', N'Tài chính',null);
@@ -56,11 +56,11 @@ insert into PHONGBAN values('PB005', N'Marketing',null);
 insert into PHONGBAN values('PB006', N'Hành chính',null);
 
 alter session set nls_date_format = 'YY-MM-DD';
-insert into DEAN values('DA001', N'HTTT quản lý các trư�?ng �?H', '2007-10-20', 'PB006');
+insert into DEAN values('DA001', N'HTTT quản lý các trường ĐH', '2007-10-20', 'PB006');
 insert into DEAN values('DA002', N'Nghiên cứu tế bào gốc', '2006-10-20', 'PB006');
-insert into DEAN values('DA003', N'Ứng dụng hóa h�?c xanh', '2003-10-10', 'PB001');
+insert into DEAN values('DA003', N'Ứng dụng hóa học xanh', '2003-10-10', 'PB001');
 insert into DEAN values('DA004', N'HTTT quản lý giáo viên cho một Khoa', '2000-10-23', 'PB003');
-insert into DEAN values('DA005', N'HTTT quản lý thư viện các trư�?ng �?H', '2009-05-10', 'PB001');
+insert into DEAN values('DA005', N'HTTT quản lý thư viện các trường ĐH', '2009-05-10', 'PB001');
 insert into DEAN values('DA006', N'Nghiên cứu chế tạo sợi Nano Platin', '2008-05-15', 'PB005');
 
 drop user admin cascade;
