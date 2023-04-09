f572c65f8d7f4847b5d4bdb753739fda35552f13
trankynhat <nhattranking@gmail.com>
2023-04-09 18:25:47 +0700

    ok

--- DoAnATBM/Global.cs
+++ DoAnATBM/Global.cs
@@ -5,8 +5,7 @@ namespace DoAnATBM
     class Global
     {
         public const string CONNECT_STRING =
-            "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.60.1)(PORT=1523)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));DBA Privilege=SYSDBA;User Id=sys;Password=sys1234;";
-
+            "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xe)));DBA Privilege=SYSDBA;User Id=sys;Password=admin;";
         public static OracleConnection CreateConnection()
         {
             var connection = new OracleConnection(CONNECT_STRING);
