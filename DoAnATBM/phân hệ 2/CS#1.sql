b64fee05bb89ad2f75c32832fd3a7efe112d48e4
huyitus <huyitus@yahoo.com>
2023-06-16 15:26:25 -0700

    mod cs1

--- DoAnATBM/phân hệ 2/CS#1.sql
+++ DoAnATBM/phân hệ 2/CS#1.sql
@@ -1,16 +1,16 @@
-CREATE OR REPLACE VIEW UV_NHANVIEN_NHANVIEN AS
+CREATE OR REPLACE VIEW ADMIN.UV_NHANVIEN_NHANVIEN AS
     SELECT *
     FROM ADMIN.NHANVIEN
     WHERE MANV = SYS_CONTEXT('USERENV', 'SESSION_USER');
 /
 
-CREATE OR REPLACE VIEW UV_NHANVIEN_PHANCONG AS
+CREATE OR REPLACE VIEW ADMIN.UV_NHANVIEN_PHANCONG AS
     SELECT *
     FROM ADMIN.PHANCONG
     WHERE MANV = SYS_CONTEXT('USERENV', 'SESSION_USER');
     
     
-CREATE OR REPLACE FUNCTION admin.nhanvien_policy (p_schema_name IN VARCHAR2, p_table_name IN VARCHAR2)
+CREATE OR REPLACE FUNCTION ADMIN.nhanvien_policy (p_schema_name IN VARCHAR2, p_table_name IN VARCHAR2)
     RETURN VARCHAR2
 AS
     v_predicate VARCHAR2(40);
@@ -20,6 +20,14 @@ BEGIN
 END;
 /
 
+BEGIN
+  DBMS_RLS.DROP_POLICY(
+    object_schema   => 'ADMIN',
+    object_name     => 'NHANVIEN',
+    policy_name     => 'STOCK_TRX_NHANVIEN_POLICY'
+  );
+END;
+/
 
 DECLARE
 BEGIN
@@ -35,20 +43,12 @@ BEGIN
 END;
 /   
 
-BEGIN
-  DBMS_RLS.DROP_POLICY(
-    object_schema   => 'admin',
-    object_name     => 'NHANVIEN',
-    policy_name     => 'STOCK_TRX_NHANVIEN_POLICY'
-  );
-END;
-
 alter session set "_oracle_script" = true;
 
 drop role NHANVIEN_ROLE;
 create role NHANVIEN_ROLE;
 
-grant SELECT ON UV_NHANVIEN_NHANVIEN TO NHANVIEN_ROLE;
+grant SELECT ON ADMIN.UV_NHANVIEN_NHANVIEN TO NHANVIEN_ROLE;
 grant SELECT ON ADMIN.PHONGBAN TO NHANVIEN_ROLE;
 grant SELECT ON ADMIN.DEAN TO NHANVIEN_ROLE;
 
@@ -56,4 +56,5 @@ drop user NV001;
 create user NV001 identified by NV001;
 grant create session to NV001;
 grant NHANVIEN_ROLE to NV001;
+
 CONN NV001/NV001;
