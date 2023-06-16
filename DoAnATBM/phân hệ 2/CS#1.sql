3cf928734e13934ce02ad808dad277189c2cb19f
huyitus <huyitus@yahoo.com>
2023-06-16 14:57:28 -0700

    mod cs1

--- DoAnATBM/phân hệ 2/CS#1.sql
+++ DoAnATBM/phân hệ 2/CS#1.sql
@@ -43,18 +43,17 @@ BEGIN
   );
 END;
 
+alter session set "_oracle_script" = true;
 
 drop role NHANVIEN_ROLE;
 create role NHANVIEN_ROLE;
 
 grant SELECT ON UV_NHANVIEN_NHANVIEN TO NHANVIEN_ROLE;
-grant SELECT ON PHONGBAN TO NHANVIEN_ROLE;
-grant SELECT ON DEAN TO NHANVIEN_ROLE;
+grant SELECT ON ADMIN.PHONGBAN TO NHANVIEN_ROLE;
+grant SELECT ON ADMIN.DEAN TO NHANVIEN_ROLE;
 
+drop user NV001;
 create user NV001 identified by NV001;
 grant create session to NV001;
 grant NHANVIEN_ROLE to NV001;
 CONN NV001/NV001;
-
-
-
